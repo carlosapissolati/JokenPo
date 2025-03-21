03a9d3405306e09640e82d766cfaed1e0ad14e6c
carlospissolati <33160767+carlospissolati@users.noreply.github.com>
2025-03-21 11:23:33 -0300

    commt inicial

---
+++ JokenPoNerd.API/Enums/ETipo.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace JokenPoNerd.API.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ETipo
+    {
+        [Description("Pedra")]
+        Pedra = 1,
+
+        [Description("Lagarto")]
+        Lagarto = 2,
+
+        [Description("Spock")]
+        Spock = 3,
+
+        [Description("Tesoura")]
+        Tesoura = 4,
+
+        [Description("Papel")]
+        Papel = 5,
+    }
+
+}
